c69a56189d3688f4df8ddbc1ce09d048f8318aa0
devendra <devendra.singh9jan@gmail.com>
2024-05-10 02:50:03 +0530

    crossorigin is not required here, if request is coming via gateway

--- src/main/java/com/mgmt/userprofile/controller/UserController.java
+++ src/main/java/com/mgmt/userprofile/controller/UserController.java
@@ -5,13 +5,13 @@ import com.mgmt.userprofile.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user")
-@CrossOrigin("http://localhost:4200")
 public class UserController {
 
     @Autowired
